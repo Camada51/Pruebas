c9b4bbbfa3b32f36d99ebcc83c2d72f22ccf8845
Camada51 <37265990+Camada51@users.noreply.github.com>
2019-02-12 10:07:06 +0100

    Add files via upload

--- eventos/20180525_Madrid/js/datosEvt.js
+++ eventos/20180525_Madrid/js/datosEvt.js
@@ -1,12 +1,16 @@
-﻿//datos del evento de Madrid del 25/05/2018
+﻿//datos del evento de Murcia del 08/02/2019
 
-nombreEvt = "20180525_Madrid";
-directorioEvt = "20180525_Madrid";
-tituloEvt = "Reunión en Madrid";
-fechaEvt = new Date("2018", "4", "25"); //año,mes,día (al mes hay que restarle 1, enero =0)
-poblacionEvt = "Madrid";
-localEvt = "Casa Baranda, c/ Colón 11";
-asistentesEvt = "Bartolomé Sánchez Bernat,Carlos Hornillos,Clemente Úbeda,Diego Soler,Eduardo Albarracín,Eduardo Toba,Francisco Llorca,José María Pertusa,José Miguel Villaescusa,Rafael Alenda,Rafael Ladrón de Guevara,Ramón Peyró";
-fotosEvt = "CasaBaranda_Bing.jpg,2018-05-25_14.45.13.jpeg,2018-05-25_16.36.06.jpeg,2018-05-25_16.36.24.jpeg,2018-05-25_16.36.48.jpeg,2018-05-25_16.37.03.jpeg,2018-05-25_17.33.22.jpeg,2018-05-25_17.33.40.jpeg,2018-05-25_17.34.08.jpeg,2018-05-25_20.19.59.jpeg";
+nombreEvt = "20190208_Murcia";
+directorioEvt = "20190208_Murcia";
+tituloEvt = "Reunión en Murcia";
+fechaEvt = new Date("2019", "1", "8"); //año,mes,día (al mes hay que restarle 1, enero =0)
+poblacionEvt = "Murcia";
+localEvt = "Restaurante La Pequeña Taberna, C/ General Margallo";
+asistentesEvt = "Rafael Ladrón de Guevara,Antonino de Mora,Salvador Ros,Diego Soler,Vicente Anguíz,José María Pertusa,Carlos Hornillos,Luis Torres,Pedro Revenga,Juan Puigcerver,Manolo Guillén,Pepe Alós,Ramón Sirvent,José Manuel Maciá,Julio Delgado,José Luis Alonso,Bartolomé Sánchez,Facundo Giménez";
+fotosEvt = "paseo_2,6Km.png,LaPequeTaberna.jpg,P1030406.JPG,P1030407.JPG,2019-02-08 at 13.33.19.jpeg,P1030410.JPG,2019-02-08 at 14.20.09_0.jpeg,P1030411.JPG,2019-02-08 at 14.20.09_01.jpeg,P1030412.JPG,2019-02-08 at 14.20.09_02.jpeg,P1030413.JPG,2019-02-08 at 14.20.09_03.jpeg,P1030414.JPG,2019-02-08 at 14.20.09_04.jpeg,P1030415.JPG,2019-02-08 at 14.20.09_05.jpeg,P1030416.JPG,2019-02-08 at 14.20.09_06.jpeg,P1030417.JPG,2019-02-08 at 14.20.09_07.jpeg,P1030418.JPG,2019-02-08 at 14.20.09_08.jpeg,P1030419.JPG,2019-02-08 at 14.20.09_09.jpeg,P1030420.JPG,2019-02-08 at 14.20.10_01.jpeg,P1030421.JPG,2019-02-08 at 14.20.10_02.jpeg,P1030422.JPG,2019-02-08 at 14.20.10_03.jpeg,P1030423.JPG,2019-02-08 at 14.20.10_04.jpeg,P1030424.JPG,2019-02-08 at 17.48.39.jpeg";
 
-menuEvt = "Fué original y variado. Hubo \"fabada con almejas\" y otras delicias.<br/><br/>Los que se acuerden que lo digan para completar.<br/><br/><br/>Rafael obsquió a los presentes con unas perdices escabechadas de factura propia que fueron muy celebradas y agradecidas.";
+menuEvt = "La mañana comenzó con un agradable paseo de más de 2 kilometros por los sitios más destacados de Murcia. De la mano de José Luis Alonso recorrimos la plaza de Santo Domingo, calle Traperias, Calle Platerías, Plaza de las Flores, Palacio Almuní, Paseo del Malecón, Ayuntamiento, Catedral y terminamos muertos de hambre en La Pequeña"+
+"El menú:<br/><br/>" +
+    "&nbsp;&nbsp;&nbsp; Aperitivos diversos de la tierra<br/>" +
+    "&nbsp;&nbsp;&nbsp; Lomo de bacalao o Dorada o Solomillo<br/>" +
+    "&nbsp;&nbsp;&nbsp; Paparojotes y mini gintonic para terminar<br/>";
